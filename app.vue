5f8e991a3ae030484e62ba7b8f52848ec344e37b
MastBlast <martin9music@gmail.com>
2023-04-06 22:23:01 +0200

    Pinia added, chart can change color

--- app.vue
+++ app.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Nav />
     <h1 class="text-3xl font-bold underline">
       DECK-1
     </h1>
+    <NuxtLink to="HoursTable" />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,5 +12,8 @@ export default {
             },
           },
         },
-    }
+    },
+    modules: [
+      '@pinia/nuxt',
+    ],
 }
