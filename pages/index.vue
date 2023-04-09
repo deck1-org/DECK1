040a06ce969a1898032729976a7ebf5cc6cb22e3
MastBlast <martin9music@gmail.com>
2023-04-09 14:41:33 +0200

    Code cleanup

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLink to="/HoursTable">
+    <h1>Go to data</h1>
+  </NuxtLink>
+</template>
+
+<style>
+h1 {
+  color: red;
+  text-align: center;
+  padding-top: 50px;
+}
+</style>
