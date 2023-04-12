1ca3197c5d2682d18cdf4fffa340d6c8354b858b
Gabriel Pędlowski <93519015+g-ped@users.noreply.github.com>
2023-04-12 20:15:18 +0200

    updated some styling

--- components/RecommendationPopUp.vue
+++ components/RecommendationPopUp.vue
@@ -3,13 +3,18 @@
     class="w-full h-full deck-frame-white content-center transition-all duration-500 ease-in-out items-center"
     :class="{ 'bg-transparent shadow-none': hideRecommendation }"
   >
-    <IconsIdea class="inline-block" />
+    <IconsIdea class="inline-block" :class="{ hidden: hideRecommendation }" />
     <p
       class="text-black pl-4 transition-all duration-500 ease-in-out"
       :class="{ 'text-transparent': hideRecommendation }"
     >
-      Click <span class="click-here">here</span> to get recommendation for
-      relocating assets to minimize costs...
+      Click
+      <span
+        class="click-here"
+        :class="{ 'text-transparent': hideRecommendation }"
+        >here</span
+      >
+      to get recommendation for relocating assets to minimize costs...
     </p>
   </div>
 </template>
