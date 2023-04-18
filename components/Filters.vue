9ede24b1851e83d4515b6ef77120166302eeb00f
DoubiOD <10407554@ucn.dk>
2023-04-18 20:19:26 +0200

    Sliders for filtering
    Sliders for filtering

--- components/Filters.vue
+++ components/Filters.vue
@@ -3,62 +3,34 @@
     <h2 class="text-lg w-full font-semibold">Filters</h2>
     <div class="pt-5 w-full flex justify-between relative">
       <label for="startHour">Working hours: </label>
-      <div>
-        <IconsInfo
-          class="cursor-pointer"
-          @mouseover="showTooltip = true"
-          @mouseout="showTooltip = false"
-        />
-        <div
-          v-show="showTooltip"
-          class="tooltip bg-gray-200 text-black p-2 absolute rounded-xl top-full right-0 shadow"
-        >
-          <p>Input fields accept</p>
-        </div>
-      </div>
     </div>
-
-    <div class="p-5 flex w-full justify-around">
-      <input
-        class="filter-input"
-        ref="inputStartHour"
-        type="number"
-        id="startHour"
-        v-model="startHour"
-      />
-      -
-      <input
-        class="filter-input"
-        ref="inputEndHour"
-        type="number"
-        id="endHour"
-        v-model="endHour"
-      />
+    <div class="p-2 flex w-full justify-around">
+      <div class="range-slider">
+        <span @change="slider">
+          <input v-model.number="startHour" ref="inputStartHour" type="number" min="0" max="23"/>
+          <input v-model.number="endHour" ref="inputEndHour" type="number" min="0" max="23"/></span>
+          <input @change="slider" v-model.number="startHour" ref="inputStartHour" min="0" max="23" step="1" type="range" />
+          <input @change="slider" v-model.number="endHour" ref="inputEndHour" min="0" max="23" step="1" type="range" />
+        <svg width="100%" height="16"></svg>
+      </div>
     </div>
     <p class="text-red-600 w-full" v-if="error">{{ errorMessage }}</p>
 
     <div class="pt-5 w-full flex justify-between relative">
-      <label for="startHour">Months from-to: </label>
+      <label for="startMonth">Months from-to: </label>
     </div>
-    <div class="p-5 flex w-full justify-around">
-      <input
-        class="filter-input"
-        ref="inputStartMonth"
-        type="number"
-        id="startMonth"
-        v-model="startMonth"
-      />
-      -
-      <input
-        class="filter-input"
-        ref="inputEndMonth"
-        type="number"
-        id="endMonth"
-        v-model="endMonth"
-      />
+    <div class="p-2 pb-8 flex w-full justify-around">
+      <div class="range-slider">
+        <span @change="slider">
+          <input v-model.number="startMonth" type="number" min="1" max="12"/>
+          <input v-model.number="endMonth" type="number" min="1" max="12"/></span>
+          <input @change="slider" v-model.number="startMonth" min="1" max="12" step="1" type="range" />
+          <input @change="slider" v-model.number="endMonth" min="1" max="12" step="1" type="range" />
+        <svg width="100%" height="16"></svg>
+      </div>
     </div>
     <button
-      class="rounded-md border-2 text-center px-2"
+      class="mx-2 my-3 p-0.5 px-3 rounded-md border-2 text-center px-2"
       @click="emitButtonClick"
     >
       Apply changes
@@ -68,7 +40,12 @@
 
 <script>
 
+import VueRangeSlider from 'vue-range-slider';
+
 export default {
+  components: {
+    VueRangeSlider,
+  },
   data() {
     return {
       startHour: ref(0),
@@ -102,7 +79,7 @@ export default {
         this.$refs.inputEndHour.value > 23 ||
         this.$refs.inputStartHour.value > 23 ||
         Number(this.$refs.inputStartHour.value) >
-          Number(this.$refs.inputEndHour.value) ||
+        Number(this.$refs.inputEndHour.value) ||
         Number(this.$refs.inputStartHour.value) % 1 !== 0 ||
         Number(this.$refs.inputEndHour.value) % 1 !== 0 ||
         this.$refs.inputEndHour.value === "" ||
