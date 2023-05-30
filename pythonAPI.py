bc1c228cd9604caecf02add1125b2ab8c51c7e72
DoubiOD <10407554@ucn.dk>
2023-05-30 15:03:35 +0200

    Python files upload

---
+++ pythonAPI.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import cds_api_fetcher
+
+appFlask = Flask(__name__)
+
+@appFlask.route("/data/<c1>/<c2>/<c3>/<c4>/<name>", methods = ['GET'])
+def getData(c1, c2, c3, c4, name):
+	write_log(name + ": " + c1 + ", " + c2 + ", " + c3 + ", " + c4 + ", " + "\n")
+	result = cds_api_fetcher.retrieve(c1, c2, c3, c4, name)
+	return result[0], result[1]
+
+if __name__ == "__main__":	
+	appFlask.run(debug=True, host="127.0.0.1", port=5555)
